3a4b93123e3f506dd2abead0c10ad832d279bd17
AllysonRivera <53790752+AllysonRivera@users.noreply.github.com>
2020-12-17 13:39:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,30 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 import requests, uuid, json
 
+#Set-up for API requests
+subscription_key = ""
+endpoint = "https://api.cognitive.microsofttranslator.com/"
+location = ""
+path = '/translate'
+constructed_url = endpoint + path
+params = {
+   'api-version': '3.0',
+   'from': 'en',
+   'to': ['es']
+}
+constructed_url = endpoint + path
+ 
+headers = {
+   'Ocp-Apim-Subscription-Key': subscription_key,
+   'Ocp-Apim-Subscription-Region': location,
+   'Content-type': 'application/json',
+   'X-ClientTraceId': str(uuid.uuid4())
+}
+ 
+body = [{
+   'text' : ""
+}]
+
 app = Flask(__name__)
 boostrap = Bootstrap(app)
 
@@ -32,3 +56,15 @@ def home():
            get_text_from_image(str(image.filename))
            return redirect(request.url)
    return render_template('homepage.html')
+
+@app.route('/<lang>')
+def translationPage(lang):
+   my_file = open('extracted.txt', 'r') 
+   my_text = my_file.read()
+   body[0]['text'] = my_text
+   params["to"][0] = lang
+   request = requests.post(constructed_url, params=params, headers=headers, json=body)
+   response = request.json()
+   t_text = response[0]["translations"][0]["text"]
+   return render_template('secondpage.html', text=my_text, t_text=t_text)
+
